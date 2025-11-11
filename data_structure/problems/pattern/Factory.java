34234247016320b3787a334ee06c89c3fd93828f
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2025-11-11 06:09:22 +0000

    Factory

---
+++ data_structure/problems/pattern/Factory.java
@@ -0,0 +1,47 @@
+package data_structure.problems.pattern;
+
+interface Payment{
+    void pay();
+}
+
+class CreditCard implements Payment{
+    public void pay(){
+        System.out.println("Credit card Payment");
+    }
+}
+
+class Upi implements Payment{
+    public void pay(){
+        System.out.println("Upi Payment");
+    }
+}
+
+abstract class PaymentFactory{
+    abstract Payment createPaymentFactory();
+}
+
+class CreditCardPaymenteFactory extends PaymentFactory{
+    public Payment createPaymentFactory(){
+        return new CreditCard();
+    }
+}
+
+class UpiPaymentFactory extends PaymentFactory{
+
+    public Payment createPaymentFactory(){
+        return new Upi();
+    }
+}
+
+public class Factory {
+    public static void main(String[] args) {
+        
+        PaymentFactory paymentFactory = new UpiPaymentFactory();
+        Payment payment = paymentFactory.createPaymentFactory();
+        payment.pay();
+    }
+}
+
+
+// Create object via method
+// Follow Open close principle
